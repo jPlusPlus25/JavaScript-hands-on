1599ed1c241d004f9a644020b94032ed5ad889dc
arshitapatel077@gmail.com <arshitapatel077@gmail.com>
2025-03-27 22:17:38 +0530

    added file app.js

---
+++ app.js
@@ -0,0 +1,17 @@
+//method chinning
+let msg='  hello ';
+let newmsg=msg.trim().toUpperCase();
+console.log(newmsg);
+//replace method
+let msg1="i love coding";
+console.log(msg1.replace("love","do"));
+console.log(msg1.replace('o','x'));
+//repeat method
+let a="mango ";
+console.log(a.repeat(6));
+//practice set ----predict
+let name="ApnaCollege";
+let predict=name;
+console.log(name.slice(4,9));
+console.log(name.indexOf("na"));
+console.log(name.replace("Apna","Our"));
