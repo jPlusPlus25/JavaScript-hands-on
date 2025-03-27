da4b8f049e542cf64155b62aada4f0641c7a95df
arshitapatel077@gmail.com <arshitapatel077@gmail.com>
2025-03-27 22:21:50 +0530

    added file stringmethods.js

---
+++ stringmethods.js
@@ -0,0 +1,12 @@
+let msg="  good  ";
+msg.trim();//"good"
+msg//"  good  "
+//let password=prompt("set your password");
+//let newpassword=password.trim();
+//console.log(newpassword);
+//index of occurrence
+let str="ilovecoding";
+console.log(str.indexOf("love"));
+console.log(str.indexOf("j"));
+console.log(str.indexOf("o"));
+console.log(str);
