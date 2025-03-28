5cdd71d45fd30d885bf12db7f7dc257cd0da3174
arshitapatel077@gmail.com <arshitapatel077@gmail.com>
2025-03-28 09:32:16 +0530

    update file

--- app2.js
+++ app2.js
@@ -12,3 +12,15 @@ let n=prompt("write your number");
 for(let i=1;i<=10;i++){
     console.log(n +"x"+i+"="+n*i);
 }
+//neasted for loop
+for(let i=1;i<=3;i++){
+    for(let j=1;j<=3;j++){
+        console.log(j);
+    }
+}
+//while loop
+let i=1;
+while(i<6){
+    console.log(i);
+    i++;
+}
