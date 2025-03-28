e920fed86ad722a07e17095b83802d40315a6437
arshitapatel077@gmail.com <arshitapatel077@gmail.com>
2025-03-28 16:42:24 +0530

    update file

--- app4.js
+++ app4.js
@@ -1,5 +1,13 @@
 //for of loop
 let fruits=["mango","apple","banana","graps"];
+
 for(fruit of fruits){
     console.log(fruit);
+}
+//nested for of loop
+let heroes =[["ironman","spiderman","thor"],["superman","wonder woman","flash"]];
+for( hero of heroes){
+    for( list of hero){
+        console.log(list);
+    }
 }
---
+++ app6.js
@@ -0,0 +1,9 @@
+const post={
+    username :"arshitapatel",
+    content : "this is my first post",
+    likes:150,
+    repost:5,
+    tags:["jaylimbasiya","rahipatel"]
+}
+console.log(post.username);
+console.log(post["username"]);
