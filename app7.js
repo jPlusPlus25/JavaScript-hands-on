9f6e7c1e6947dd3ca939aa69ba5e9759882f6865
arshitapatel077@gmail.com <arshitapatel077@gmail.com>
2025-04-01 17:20:11 +0530

    update file

--- app7.js
+++ app7.js
@@ -93,5 +93,14 @@ function oddOrEven(request){
     }else{
         console.log("not even or not odd");
     }
-}
+}const calculator={
+    num:55,
+    add:function(a,b){
+        return a+b;
+    },sub:function(a,b){
+        return a-b;
+    },mul:function(a,b){
+        return a*b;
+    }
+};
 
