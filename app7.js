fcbb4335f18136b65ab8995f7afd5a20c84e84ba
arshitapatel077@gmail.com <arshitapatel077@gmail.com>
2025-04-02 11:12:22 +0530

    update file

--- app7.js
+++ app7.js
@@ -132,15 +132,15 @@ console.log("hii jay");
 //    console.log("Arshita")
 //},4000);
 //console.log("WelCome");
-let id1=setInterval(()=>{
-    console.log("Arshita")
-},4000);
-clearInterval(id);
-let id=setInterval(()=>{
-    console.log("Arshita")
-},4000);
-console.log(id);
-clearInterval(id1);
+//let id1=setInterval(()=>{
+//    console.log("Arshita")
+//},4000);
+//clearInterval(id);
+//let id=setInterval(()=>{
+//    console.log("Arshita")
+//},4000);
+//console.log(id);
+//clearInterval(id1);
 const student1={
     name:"Jay",
     marks:91,
