941ab4ddb0b22770f37cdff321ea5b25000c0b51
arshitapatel077@gmail.com <arshitapatel077@gmail.com>
2025-04-03 13:17:56 +0530

    update file

--- app10.js
+++ app10.js
@@ -14,4 +14,17 @@ let data={
 };
 let datacopy={...data,id:123};//{email: 'arshitapatel077@gmail.com', password: 'abc123', id: 123}
 let datacopy1={...data,id:123,country:"India"};//{email: 'arshitapatel077@gmail.com', password: 'abc123', id: 123, country: 'India'}
+let arr1=[1,3,2,6,5,8,5,4];//val
+let obj1={...arr1};//obj ->key:val
+let obj2={..."hello"}//onj ->key:val
+//rest method
+function sum(...args){
+    for(let i=0;i<args.length;i++){
+        console.log("you gave us: ",args[i]);
+    }
+}
+//destructuring method
+let names=["Arshita","Jay","Rutvi","Rahi","Monika"];
+let [winner,runnerup,secondRunnerup]=names;
+
 
