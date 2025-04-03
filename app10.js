a9a26fcc3d8901b7eaa27e8a0195fe104a8b7e2a
arshitapatel077@gmail.com <arshitapatel077@gmail.com>
2025-04-03 10:49:30 +0530

    update file

--- app10.js
+++ app10.js
@@ -3,5 +3,8 @@ let arr=[1,3,2,6,5,8,5,4];
 let newArr=[...arr];
 console.log(newArr);
 let chars=[..."hello"];
-
+let odd=[1,3,5,7,9];
+let even=[2,4,6,8,10];
+let num=[...odd,...even];
+console.log(num);//(10) [1, 3, 5, 7, 9, 2, 4, 6, 8, 10]
 
