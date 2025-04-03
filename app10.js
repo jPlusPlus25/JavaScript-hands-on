89d50a05f1cd0cb521426e95967d52fb32f9c363
arshitapatel077@gmail.com <arshitapatel077@gmail.com>
2025-04-03 11:01:28 +0530

    update file

--- app10.js
+++ app10.js
@@ -7,4 +7,11 @@ let odd=[1,3,5,7,9];
 let even=[2,4,6,8,10];
 let num=[...odd,...even];
 console.log(num);//(10) [1, 3, 5, 7, 9, 2, 4, 6, 8, 10]
+//spread with object literals
+let data={
+    email:"arshitapatel077@gmail.com",
+    password:"abc123",
+};
+let datacopy={...data,id:123};//{email: 'arshitapatel077@gmail.com', password: 'abc123', id: 123}
+let datacopy1={...data,id:123,country:"India"};//{email: 'arshitapatel077@gmail.com', password: 'abc123', id: 123, country: 'India'}
 
