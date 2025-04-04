f8b1212c19652560fcf15df9c97be82d449c980b
arshitapatel077@gmail.com <arshitapatel077@gmail.com>
2025-04-04 11:13:15 +0530

    update file

--- app10.js
+++ app10.js
@@ -40,5 +40,24 @@ let btn = document.querySelector("button");
 console.dir(btn);
 btn.onclick=function(){
     alert("button was clicked");
+};
+let btns=document.querySelectorAll("button");
+for (btn of btns){
+   // btn.onclick=sayHello;
+    //btn.onmouseenter=function(){
+     //   console.log("you entered a button");
+    //}
+    //console.dir(btn);
+    btn.addEventListener("click",sayHello);
+    btn.addEventListener("click",sayName);
 }
+
+function sayHello(){
+    alert("Hello");
+}
+function sayName(){
+    alert("how are u?")
+}
+btn.onclick=sayHello;
+
 
