27dc1a0e194f1c483ceda3f29947d644fbb725db
arshitapatel077@gmail.com <arshitapatel077@gmail.com>
2025-04-04 15:44:28 +0530

    update file

--- app11.js
+++ app11.js
@@ -1,20 +1,28 @@
-let btn=document.querySelector("button");
+//let btn=document.querySelector("button");
 
-btn.addEventListener("click",function(){
-    let h3=document.querySelector("h3");
-    let randomColor=getRandomColor();
-    h3.innerText=randomColor;
+//btn.addEventListener("click",function(){
+  //  let h3=document.querySelector("h3");
+    //let randomColor=getRandomColor();
+    //h3.innerText=randomColor;
 
-    let div=document.querySelector("div");
-    div.style.backgroundColor=randomColor;
+    //let div=document.querySelector("div");
+    //div.style.backgroundColor=randomColor;
 
-    console.log("color updated");
-});
-function getRandomColor(){
-    let red=Math.floor(Math.random()*255);
-    let green=Math.floor(Math.random()*255);
-    let blue=Math.floor(Math.random()*255);
+    //console.log("color updated");
+//});
+//function getRandomColor(){
+   // let red=Math.floor(Math.random()*255);
+    //let green=Math.floor(Math.random()*255);
+    //let blue=Math.floor(Math.random()*255);
 
-    let color='rgb(${red}, ${green}, ${blue})';
-    return color;
-}
+    //let color='rgb(${red}, ${green}, ${blue})';
+    //return color;
+    let p=document.querySelector("p");
+    p.addEventListener("click",function(){
+        console.log("parah was clicked");
+    });
+    let box=document.querySelector(".box");
+    box.addEventListener("mouseenter",function(){
+        console.log("mouse inside box")
+    });
+//}
