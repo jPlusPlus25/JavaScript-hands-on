8a916ab2d23e509a8ada2ef4a8c1c76bcbd0d7bc
arshitapatel077@gmail.com <arshitapatel077@gmail.com>
2025-04-05 19:11:11 +0530

    update file

--- eventl.js
+++ eventl.js
@@ -1,19 +1,9 @@
-//event listenrt
-function myFunction(){
-    console.log("i was onclicked");
-}
-const mouseOver = () =>{   //arrow function use krine function bnaviyu
-    console.log("i am onmouseover.");
-}
-function myFunction2(){
-    console.log("i am onload.")
-}
-const onKeyPress=()=>{
-    console.log(" i am onkeypress");
-}
-function onKeyDown(){
-    console.log("i am onKeyDown");
-}
-function onKeyUp(){
-    console.log("i am onKeyUp()");
-}
+//event listenrs
+const box1=document.getElementById('box-1');
+box1.addEventListener('click',()=>{
+    console.log('click by event');
+});
+
+box1.addEventListener('click',()=>{
+    console.log('click by event 2');
+});
