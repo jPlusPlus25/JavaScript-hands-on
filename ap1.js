fa19850b1429db1ced8afc8e35926ad5a6575c9e
arshitapatel077@gmail.com <arshitapatel077@gmail.com>
2025-04-05 18:54:20 +0530

    added file

---
+++ ap1.js
@@ -0,0 +1,18 @@
+function myFunction(){
+    console.log("i was onclicked");
+}
+const mouseOver = () =>{   //arrow function use krine function bnaviyu
+    console.log("i am onmouseover.");
+}
+function myFunction2(){
+    console.log("i am onload.")
+}
+const onKeyPress=()=>{
+    console.log(" i am onkeypress");
+}
+function onKeyDown(){
+    console.log("i am onKeyDown");
+}
+function onKeyUp(){
+    console.log("i am onKeyUp()");
+}
