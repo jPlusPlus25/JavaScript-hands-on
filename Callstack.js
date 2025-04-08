4830b3bac1d70c31c57ad3009ab4986ccd1cd732
arshitapatel077@gmail.com <arshitapatel077@gmail.com>
2025-04-08 20:44:24 +0530

    update file

--- Callstack.js
+++ Callstack.js
@@ -1,7 +1,24 @@
 function hello(){
+    console.log("inside hello fnc");
     console.log("hello");
 }
 function demo(){
+    console.log("calling hello fnc");
     hello();
 }
+console.log("calling demo fnc");
 demo();
+console.log("done, bye!");
+
+//visualizing the call stack
+function one(){
+    return 1;
+}
+function two(){
+    return one()+one();
+}
+function three(){
+    let ans=two()+one();
+    console.log(ans);
+}
+three();
