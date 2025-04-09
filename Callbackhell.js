32f31924a2fde924c25303f2cd2ab94d8d19a007
arshitapatel077@gmail.com <arshitapatel077@gmail.com>
2025-04-09 10:17:42 +0530

    update file

--- Callbackhell.js
+++ Callbackhell.js
@@ -11,11 +11,29 @@ changeColor("red",1000,()=>{
     });
 });
 //callback ki nesting ko callbackhell kehte he*/
-function savetoDb(data){
+/*function savetoDb(data){
     let internetSpeed = Math.floor(Math.random()*10)+1;
     if(internetSpeed>4){
         console.log("your data was saved :",data);
     }else{
         console.log("weak connection. data not saved");
     }
+}*/
+
+function savetoDb(data,succsess,failure){
+    let internetSpeed = Math.floor(Math.random()*10)+1;
+    if(internetSpeed>4){
+        succsess();
+    }else{
+        failure();
+    }
 }
+savetoDb(
+    "SlTIET",
+    ()=>{
+        console.log("succsess:your data was saved");
+    },
+    ()=>{
+        console.log("failure:weak connection. data not saved");
+    }
+);
