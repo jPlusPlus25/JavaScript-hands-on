b385d1547f9bcf36c81b530e819ea296d5b76975
arshitapatel077@gmail.com <arshitapatel077@gmail.com>
2025-04-09 15:05:09 +0530

    update file

--- Callbackhell.js
+++ Callbackhell.js
@@ -96,4 +96,12 @@ function savetoDb(data){
 //async function
 async function greet(){
     return "hello!";
-}            
+}  
+greet()
+.then((result)=>{
+    console.log("promise was resolved");
+    console.log("result was :",result);
+})          
+.catch((err)=>{
+    console.log("promise was rejected with err :",err);
+});
